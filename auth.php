ea1be3c460ae2925775d192cf2b55ba80f0d6cc7
d1ssimilate <131668270+d1ssimilate@users.noreply.github.com>
2023-04-27 18:47:19 +0400

    Add files via upload

---
+++ auth.php
@@ -0,0 +1,31 @@
+<?php
+    include 'controllers/db.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AMHLOPHE</title>
+    <link rel="stylesheet" href="../сss/auth.css">
+</head>
+<body>
+    <div class="auth">
+        <!----------------ФОРМА АВТОРИЗАЦИИ---------------->
+        <div class="auth_form">
+            <p class="auth_text">Авторизация</p>
+            <form action="controllers/auth.php" method="POST">
+                <input type="text" name="mail" placeholder="Почта" class="mail_pass">
+                <input type="text" name="pass" placeholder="Пароль" class="mail_pass">
+        <?php 
+            echo "$_SESSION[mess_error]";
+            unset($_SESSION['mess_error']);
+                ?>
+                <input type="submit" name="auth" value="Войти" class="auth_but"> 
+                <p class="create_acc">Нет аккаунта? <a href="registration.php">Создайте его</a></p>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
---
+++ registration.php
@@ -0,0 +1,38 @@
+<?php
+    include 'controllers/db.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AMHLOPHE</title>
+    <link rel="stylesheet" href="../сss/reg.css">
+</head>
+<body>
+    <div class="auth">
+        <!----------------ФОРМА РЕГИСТРАЦИИ---------------->
+        <div class="auth_form">
+            <p class="auth_text">Регистрация</p>
+                <?php 
+                    echo "$_SESSION[mess_error]";
+                    unset($_SESSION['mess_error']);
+                ?>
+            <div class="form_text">
+            <form action="controllers/registration.php" method="POST">
+                <input type="text" name="name" placeholder="Имя" class="text">
+                <input type="text" name="surname" placeholder="Фамилия" class="text">
+                <input type="text" name="l_name" placeholder="Отчество" class="text">
+                <input type="number" name="phone" placeholder="Номер телефона" class="text">
+                <input type="password" name="pass" placeholder="Пароль" class="text">
+                <input type="password" name="repass" placeholder="Повторите пароль" class="text">
+                <input type="email" name="mail" placeholder="Почта" class="text">
+                <input type="date" name="birthday" placeholder="Дата рождения" class="text">
+                <input type="submit" name="auth" value="Зарегистрироваться" class="auth_but">
+            </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
